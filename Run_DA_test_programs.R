5499807dc0ba82777645380685a21be49302932a
mingkaijiang <shanyujordan512@gmail.com>
2018-03-08 08:41:58 +1100

    structured the repo

---
+++ Run_DA_test_programs.R
@@ -0,0 +1,23 @@
+#### Master script 
+#### To understand and test EnKF function 
+
+###### ---------------- setting up runs -------------------- ######
+#### clear wk space
+rm(list=ls(all=TRUE))
+
+#### Source functions and packages
+source("include/prepare.R")
+
+#### turn warnings off globally
+# options(warn=-1)
+
+###### ---------------- Create fake data -------------------- ######
+
+
+###### ---------------- Calling EnKF functions -------------------- ######
+
+
+###### ---------------- End -------------------- ######
+#### clear wk space
+rm(list=ls(all=TRUE))
+options(war=0)
---
+++ include/prepare.R
@@ -0,0 +1,11 @@
+
+if(!require(pacman))install.packages("pacman")
+pacman::p_load(data.table,
+               ggplot2,
+               knitr) # add other packages needed to this list
+
+# Sourcing all R files in the modules subdirectory
+sourcefiles <- dir("DA_Functions", pattern="[.]R$", recursive = TRUE, full.names = TRUE)
+for(z in sourcefiles)source(z)
+
+
