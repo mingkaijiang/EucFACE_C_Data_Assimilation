98d3162aeb6c07281a1553e9d22e83fff9426989
mingkaijiang <shanyujordan512@gmail.com>
2018-03-29 13:28:56 +1100

    re-organized the folder

--- DA_Functions/analysis.R
+++ DA_Functions/analysis.R
@@ -1,133 +1,191 @@
-analysis <- function(A, s, obs, i) {
+analysis <- function(A, s, obs, i,
+                       err_var, err_type, 
+                       err_var_obs, err_type_obs, 
+                       ens_var, q) {
     
     ### The standard analysis eqn: A = A + Pe H^T(H Pe H^T + Re)^-1 (D - H A)    (eq 48)
     ### can be reformed using D' = D - HA, Pe = A'(A')^T, Re = YY^T
     ### (where Y symbolises Gamma) such that it
     ### becomes A = A + A' A'^T H^T(HA' A'^T H^T + YY^T)^-1 D'
     
-    sig_sum = 0.0
-    sig_sum1 = 0.0
+    ## Define matrix and vector
+    nrmin <- min(nrobs, s$nrens)         # Minimum of nrobs and nrens
+    nrsigma <- 0
+    sigsum <- 0
+    sigsum1 <- 0
     
-    # Minimum of nrobs and nrens
-    nrmin = min(s$nrobs+1, s$nrens)
+    I <- diag(1, s$nrens,s$nrens)        # identity matrix
+    U <- matrix(0, nrobs,nrmin)          # local variable
+    D <- matrix(0, nrobs,s$nrens)        # matrix holding innovations
+    S <- matrix(0, nrobs,s$nrens)        # matrix holding product of HA'
+    E <- matrix(0, nrobs,s$nrens)        # Matrix holding observation uncertainty
+    H <- matrix(0, nrobs,s$ndims)        # Matrix holding observation operator
+    HA <- matrix(0, nrobs,s$nrens)      
+    ES <- matrix(0, nrobs,s$nrens)       # Matrix holding product of HA' + E
+    X1 <- matrix(0, nrmin,nrobs)         # local variable
+    X2 <- matrix(0, nrmin,s$nrens)       # local variable
+    X3 <- matrix(0, nrobs,s$nrens)       # local variable
+    X4 <- matrix(0, s$nrens,s$nrens)     # local variable
+    A_tmp <- matrix(0, s$ndims,s$nrens)
+    A_dash <- matrix(0, s$ndims,s$nrens)
+    Reps <- matrix(0, s$ndims,nrobs) 
+    VT <- matrix(0, s$nrens, s$nrens)
     
-    I = matrix(0, s$nrens,s$nrens)
-    U = matrix(0, s$nrobs,nrmin)
-    D = matrix(0, s$nrobs,s$nrens)
-    S = matrix(0, s$nrobs,nrmin)
-    E = matrix(0, s$nrobs,nrmin)
-    H = matrix(0, s$nrobs,s$ndims)
-    HA = matrix(0, s$nrobs,s$nrens)
-    ES = matrix(0, s$nrobs,s$nrens)
-    X1 = matrix(0, nrmin,s$nrens)
-    X2 = matrix(0, nrmin,s$nrens)
-    X3 = matrix(0, s$nrobs,s$nrens)
-    X4 = matrix(0, s$nrens,s$nrens)
-    Reps = matrix(0, s$ndims,s$nrobs)
-    A_tmp = matrix(0, s$ndims,s$nrens)
-    A_dash = matrix(0, s$ndims,s$nrens)
+    sig <- rep(0, nrmin)
+    D_mean <- rep(0, nrobs)
+    E_mean <- rep(0, nrobs)
+    HA_mean <- rep(0, nrobs)
+    A_mean <- rep(0, s$ndims)
     
-    sig = seq(0, nrmin)
-    D_mean = seq(0, s$nrobs)
-    E_mean = seq(0, s$nrobs)
-    S_mean = seq(0, s$nrobs)
-    HA_mean = seq(0, s$nrobs)
-    A_mean = seq(0, s$nrobs)
-
-    # Compute HA' + E
-    ES = S + E
-    
-    # Compute SVD of HA'+E -> U and sig, using Eispack
-    U <- svd(ES)
-    
-    # # A = U*Σ*VT for real routines
-    # dgesvd (’S’,           # jobu: the first min(m, n) columns of U (the left singular vectors) are returned in the array u;
-    #          ’N’,          # if jobvt = 'N', no rows of VT/VH (no right singular vectors) are computed.
-    #          nrobs,        # The number of rows of the matrix A (m≥ 0).
-    #          nrens,        # The number of columns in A (n≥ 0).
-    #          ES,           # a(lda,*) is an array containing the m-by-n matrix A.
-    #          nrobs,        # lda: The leading dimension of the array a.
-    #          sig,          # s: Array, size at least max(1, min(m,n)). Contains the singular values of A sorted so that s(i) ≥ s(i+1).
-    #          U,            # u(ldu,*); the second dimension of u must be at least max(1, min(m, n)) if jobu = 'S'.
-    #          nrobs,        # The leading dimensions of the output arrays u and vt, respectively.
-    #          V,            # The dimension of the array work.
-    #          nrens,        # If jobvt = 'N'or 'O', vt is not referenced.
-    #          work,         # work is a workspace array, its dimension max(1, lwork)
-    #          lwork,        # The dimension of the array work.
-    #          ierr)         # If info = 0, the execution is successful.
-    
-    for (i in c(1:nrmin)) {
-        sig[i] <- sig[i] ** 2
+    ## Generate the H matrix: gsl_matrix_set(H, j, k, (o + j)->obsop[k]);
+    for (j in 1:nrobs) {
+        for (k in 1:s$ndims) {
+            H[j,k] <- obsop[k]   # This should be a pointer operator function
+        }
     }
-         
 
-    sigsum = sum(sig)
-    sigsum1 = 0.0
-    nrsigma = 0
-    for (i in c(1:nrmin)) {
-        if (sigsum1/sigsum < 0.999) {
-            nrsigma = nrsigma + 1
-            sigsum1 = sigsum1 + sig[i]
-            sig[i] = 1.0/sig[i]
+    ## Compute HA
+    HA <- H %*% A
+    
+    ## Calculate observation uncertainty (defined as gamma in Eversen 2003)
+    for (j in 1:nrobs) {
+        if (err_type_obs[8] == 0) {
+            E[j,] <- rnorm(s$nrens, mean=0, sd=err_var_obs[8,i]) 
         } else {
-            sig[i:nrmin] = 0.0
+            E[j,] <- rnorm(s$nrens, mean=obs[8,j], sd=abs(obs[8,j] * err_var_obs[8,i])) 
         }
     }
-
-    # Compute X1
-    for (j in c(1:s$nrobs)) {
-        for (k in c(1:nrmin)) {
-            X1[k, j] <- sig[k] * U$v[j, k]
+    E_mean <- rowMeans(E, na.rm=T)
+    
+    
+    ## Compute the innovations, D', where D' = D - HA. eqn 53 evenson 2003 
+    for (j in 1:nrobs) {
+        for (k in 1:s$nrens) {
+            ## Add the observation uncertainty to observation (eqn 48 Evenson 2003) before taking HA away from it */
+            D[j,k] <- E[j,k] + obs[8,j] - HA[j,k]
         }
     }
+    D_mean <- rowMeans(D, na.rm=T)
+    
+    
+    ### Calculate HA' by taking mean_HA from HA. 
+    ### H must be linear!  
+    ### Store in matrix S as Evenson does. 
+    ### Point 5 section 5.2 page 356 Evenson 2003. 
 
-    # Compute X2 = X1 * D
-    # multiplying matrices
-    # dgemm(’n’,                 # Character indicating that the matrices A and B should not be transposed or conjugate transposed before multiplication.
-    #        ’n’, 
-    #        nrmin,              # M: A: M rows by K columns
-    #        nrens,              # N: B: K rows by N columns
-    #        nrobs,              # K: C: M rows by N columns
-    #        1.0,                # Real value used to scale the product of matrices A and B.
-    #        X1,                 # Array used to store matrix A.
-    #        nrmin,              # Leading dimension of array A, or the number of elements between successive columns (for column major storage) in memory. In the case of this exercise the leading dimension is the same as the number of rows.
-    #        D,                  # Array used to store matrix B.
-    #        nrobs,              # Leading dimension of array B, 
-    #        0.0,                # Real value used to scale matrix C.
-    #        X2,                 # Array used to store matrix C
-    #        nrmin)              # Leading dimension of array C
+    ## First figure out mean value of HA 
+    HA_mean <- rowMeans(HA, na.rm=T)
+    
+    ## HA' = HA - mean_HA 
+    for (j in 1:nrobs) {
+        for (k in 1:s$nrens) {
+            S[j,k] <- HA[j,k] - HA_mean[j]
+        }
+    }
+    
+    ## Compute HA' + E (evenson page 356) , Ha' stored in S...so ES = S + E 
+    ES <- S + E
+    
+    ### Compute SVD of HA'+E store in U, eqn 59 and pg 357 evenson example 2003
+    ### LAPACK bits for SVD
+    #lwork = 2 * max(3 * s$nrens + nrobs, 5 * s$nrens)
+    #
+    ## SVD: ES = U * SIGMA * V^T
+    ## ES: m * n,     i.e. nrobs * nrens
+    ## SIGMA: m * n, all zero except diagnomal elements
+    ## U: m * m
+    ## V^T: (V transposed) is an n * n orthogonal matrix
+    ## call dgesvd(jobu, jobvt, m, n, a, lda, s, u, ldu, vt, ldvt, work, lwork, info)
+    # dgesvd_("S",        # the first min(m,n) columns of U (the left singular vectors) are returned in the array u
+    #         "N",        # no rows of V^T/V^H (no right singular vectors) are computed
+    #         nrobs,      # number of rows of the matrix ES
+    #         s$nrens,    # number of columns in ES 
+    #         es,         # array containing the m * n matrix ES, the 2nd dimension of a must be at least max (1,n)
+    #         nrobs,      # the leading dimension of the array es
+    #         sig,        # Contains the singular values of ES sorted so that sig (i) >= sig(i+1)
+    #         U,          # m by m output
+    #         nrobs,      # the leading dimension of the output array u 
+    #         VT,          # should be n by n, why we have n by m here?
+    #         inrens,     # the leading dimension of the output array v
+    #         work_,      # workspace array, its dimension max(1, lwork)
+    #         lwork,      # the dimension of the array work
+    #         ierr)       # output
+    # 
+    out <- svd(ES, nu = min(nrobs, s$nrens), nv = min(nrobs, s$nrens))
+    U <- out$u   
+    VT <- out$v
+    sig <- out$d
+
+    ## Convert to eigenvalues and work out sigsum - pg 357 evenson example 2003
+    for (j in 1:nrmin) {
+        sig[j] <- sig[j]^2
+        sigsum <- sigsum + sig[j]
+    }
+    
+    ## Compute number of significant eigenvalues - pg 357 evenson example 2003
+        for (j in 1:nrmin) {
+            if ((sigsum1 / sigsum) < 0.999) {
+                nrsigma <- nrsigma + 1
+                sigsum1 <- sigsum1 + sig[j]
+                sig[j] <- 1.0 / sig[j]
+            } else {
+                for (k in j:nrmin) {
+                    sig[k] <- 0.0 
+                }
+            }
+        }
+    
+    ## Compute X1 = sig * U - pg 357 evenson example 2003
+        for (j in 1:nrobs) {
+            for (k in 1:nrmin) {
+                X1[k,j] <- sig[k] *  U[j,k] 
+            }
+        }
+    
+    ## Compute X2 = X1 * D - pg 357 evenson example 2003 
     X2 <- X1 %*% D
 
-    # Compute X3 = U * X2
-    # dgemm(’n’, ’n’, nrobs, nrens, nrmin, 1.0, U, nrobs, X2, nrmin, 0.0, X3, nrobs)
+    ## Compute X3 = U * X2 - pg 357 evenson example 2003
     X3 <- U %*% X2
-    
-    # Compute final analysis
-    if(2 * ndim * nrobs > nrens * (nrobs+ndim)) {
-        # dgemm(’t’, ’n’, nrens, nrens, nrobs, 1:0, S, nrobs, X3, nrobs, 0.0, X4, nrens)
-        X4 <- S %*% X3
-        for (i in 1:s$nrens) {
-            X4[i,i] = X4[i,i] + 1.0
+
+    ## Compute final analysis - pg 357 evenson example 2003
+    ## X4 = (HA')^T * X3
+    ## X5 = X4 +I
+    ## A = A + A' * X5
+    if ((2 * s$ndims * nrobs) > (s$nrens * (nrobs + s$ndims))) {
+        ## compute X4 = (HA')^T * X3 - note S matrix is HA' 
+        X4 <- t(S) %*% X3
+
+        ## Compute X5 = X4 + I (store in X4) 
+        for (k in 1:s$nrens) {
+            X4[k,k] <- X4[k,k] + 1.0;
+        }
+        X4 <- X4 + I
+        
+        ## Compute A = A * X5 (note X5 stored in X4 -> see Evenson) */
+        A_tmp <- A
+        A <- A_tmp %*% X4
+        
+    } else {
+        ## Compute representers Reps = A' * S^T
+        ## Calculate the mean of the ensemble required to work out A' 
+        A_mean <- rowMeans(A, na.rm=T)
+        
+        ## Figure out A' 
+        for (j in 1:s$ndims) {
+            for (k in 1:s$nrens) {
+                A_dash[j,k] <- A[j,k] - A_mean[j]
+            }
         }
         
-        iblkmax = min(s$ndim, 200)
-        multa(A, X4, ndim, nrens, iblkmax)
-    } else {
-        #dgemm(’n’, ’t’, ndim, nrobs, nrens, 1.0, A, ndim, S, nrobs, 0.0, Reps, ndim)
-        Reps <- A %*% S
-        #dgemm(’n’, ’n’, ndim, nrens, nrobs, 1.0, Reps, ndim, X3, nrobs, 1.0, A, ndim)
-        A <- Reps %*% X3
+        ## Compute representers Reps = A' * S^T 
+        Reps <- A_dash %*% t(S)
+        
+        ## Compute A = A + Reps * X3 
+        A_tmp <- Reps %*% X3
+        A <- A + A_tmp
     }
+    
+    return(A)
 }
     
-
-
-multa <- function(A, X, ndim, nrens, iblkmax) {
-    for (ia in seq(1, ndim, iblkmax)) {
-        ib = min(ia + iblkmax - 1, ndim)
-        v(1:(ib - ia + 1), 1:nrens) = A(ia:ib, 1:nrens)
-        #dgemm(’n’, ’n’, ib - ia + 1, nrens, nrens, 1.0, v(1,1), iblkmax, X(1,1), nrens,0.0, A(ia, 1), ndim)
-        A[ia, 1] <- v[1,1] %*% X[1,1]
-    }
-
-}
--- DA_Scripts/EnKF_framework_William_example.R
+++ DA_Scripts/EnKF_framework_William_example.R
@@ -1,3 +1,6 @@
+#### This is a model built based on Martin's code logic
+#### and will need to add R code specifically on data assimilation 
+#### to complete the framework - specifically within analysis function
 
 ####---- Clear the console ----####
 rm(list=ls(all=TRUE))
@@ -31,23 +34,18 @@ err_var <- initialise_error_variance(s, err_var)
 err_type <- initialise_error_type(s, err_type)
 
 ####----  Set up the observation stuffs ----####
-obs <- c()
-### Create the observational matrix, for each state and day
-#B <- matrix(NA, s$ndims, ndays)
-#
-#### intialize measurement noise matrix
-#q_obs <- matrix(rnorm(s$ndims*ndays, 0.0, 1.0), s$ndims, ndays)
-#    
-#### initialize measurement matrix
-#B <- initialise_observation(p, s, B)
-#
+obsDF <- read.csv("obs_cf_1.csv", header=F)
+nrobs <- ncol(obsDF)-1
+obs <- as.matrix(obsDF[,2:ncol(obsDF)])
+obsop <- initialise_obs_operator()
+#nrobs <- initialise_nrobs(obs)
+
 #### initialize measurement error variance and type
-#err_var_obs <- matrix(0, s$ndims, ndays)
-#err_type_obs <- rep(0, s$ndims)
-#ens_var_obs <- matrix(0, s$ndims, ndays)
-#
-#err_var_obs <- initialise_obs_error_variance(s, err_var_obs)
-#err_type_obs <- initialise_obs_error_type(s, err_type_obs)
+err_var_obs <- matrix(0, s$ndims, ndays)
+err_type_obs <- rep(0, s$ndims)
+
+err_var_obs <- initialise_obs_error_variance(s, err_var_obs)
+err_type_obs <- initialise_obs_error_type(s, err_type_obs)
 
 
 ####----  set up storage df to store the simulation output, with uncertainties ----####
@@ -69,24 +67,19 @@ for (i in 1:ndays) {
                     err_type, ens_var, q)
     
     ## split the list of the forecast model
-    A <- out$A
-    ens_var <- out$ens_var
-    q <- out$q
+    A <- out$A                 # model prediction ensemble member
+    ens_var <- out$ens_var     # model variance
+    q <- out$q                 # model error
     
-    ## Recalcualte model forecast where observations are avaliable
-    ## need to consider obs at timestep i
-    # if (s$nrobs > 0) {
-    #     analysis(A, s, obs)
-    # }
+    A <- analysis(A, s, obs, i, 
+                  err_var, err_type, 
+                  err_var_obs, err_type_obs, 
+                  ens_var, q)
     
-    # Save output
+    ## Save output
     ensembleDF[i, 2:(s$ndims*2+1)] <- dump_output(s, A)
     
 }
-
-### this is the predicted values
-sim <- t(as.matrix(ensembleDF[,c("RA", "AF", "AW", "AR", "LF", "LW", "LR",
-                               "CF", "CW", "CR", "RH1", "RH2", "D", "CL", "CS", "GPP")]))
 
 
 ####----  Plotting ----####
@@ -100,8 +93,8 @@ colnames(obsDF) <- c("Days", "RA", "AF", "AW", "AR", "LF", "LW", "LR",
                           "CW_STDEV", "CR_STDEV", "RH1_STDEV", "RH2_STDEV", 
                           "D_STDEV", "CL_STDEV", "CS_STDEV", "GPP_STDEV")
 obsDF$Days <- c(1:ndays)
-obsDF[,2:(s$ndims+1)] <- as.data.frame(t(B))
-obsDF[,(s$ndims+1):ncol(obsDF)] <- as.data.frame(t(B)*t(err_var_obs))
+obsDF$CF <- mean(obs[s$POS_CF,], na.rm=T)
+obsDF$CF_STDEV <- sd(obs[s$POS_CF,], na.rm=T)
 
 
 ### plotting    
@@ -109,6 +102,6 @@ ggplot() +
     geom_ribbon(data=ensembleDF, aes(x = Days, ymin=CF-CF_STDEV, 
                   ymax=CF+CF_STDEV), fill="grey", alpha=1) +
     geom_line(data=ensembleDF, aes(y = CF, x=Days), color = "black") +
-    geom_point(data=obsDF, aes(y = CF, x=Days), color="red") +
-    geom_errorbar(data=obsDF, aes(ymin=CF-CF_STDEV, ymax=CF+CF_STDEV, x=Days), 
-                  width=0.1, position=position_dodge(0.05), color="brown")
+    geom_point(data=obsDF, aes(y = CF, x=Days), color="red")  #+
+    #geom_errorbar(data=obsDF, aes(ymin=CF-CF_STDEV, ymax=CF+CF_STDEV, x=Days), 
+    #              width=0.1, position=position_dodge(0.05), color="brown", alpha=1)
